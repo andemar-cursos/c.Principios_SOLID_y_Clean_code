a5ed2b5590bc9303400d1e977305464546b6691b
Anderson <andemartinezdev@gmail.com>
2022-05-01 12:37:15 -0500

    02-09 -> Ejercicio con funciones
    - Se cambio el nombre de las funciones para dar una idea mas clara de lo que van a hacer.

---
+++ src/clean-code/03-funciones.ts
@@ -0,0 +1,39 @@
+(() => {
+  // función para obtener información de una película por Id
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieActorsById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBioById(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  interface movie {
+    title: string;
+    description: string;
+    rating: string;
+    cast: string[];
+  }
+
+  // Crear una película
+  function createMovie({ title, description, rating, cast }: movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === 'fernando') return false;
+
+    console.log('Crear actor');
+    return true;
+  }
+})();
