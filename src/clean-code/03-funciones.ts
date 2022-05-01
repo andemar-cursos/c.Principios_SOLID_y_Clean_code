a6db45f11af28562164e385991d0da111da8b2f7
Anderson <andemartinezdev@gmail.com>
2022-05-01 13:55:54 -0500

    02-12 -> Refactorizacion funciones
    - Se hizo la refactorizacion de funciones las cuales no eran claras al momento del leer el codigo.

--- src/clean-code/03-funciones.ts
+++ src/clean-code/03-funciones.ts
@@ -37,40 +37,33 @@
     return true;
   }
 
+  // Continuacion
 
-    // Continuacion
+  const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+    let result: number;
 
-    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
-        
-        let result : number;
-        
-        if ( isDead ) {
-            result = 1500;
+    if (isDead) {
+      result = 1500;
+    } else {
+      if (isSeparated) {
+        result = 2500;
+      } else {
+        if (isRetired) {
+          result = 3000;
         } else {
-            if ( isSeparated ) {
-                result = 2500;
-            } else {
-                if ( isRetired ) {
-                    result = 3000;
-                } else {
-                    result = 4000; 
-                }
-            }
+          result = 4000;
         }
-        
-        return result;
+      }
     }
 
-    const getPayAmountRefactor = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
-        
-        if ( isDead ) return 1500;
-            
-        if ( isSeparated ) return 2500;
-        
-        return ( isRetired ) ? 3000 : 4000;
-    }
+    return result;
+  };
 
+  const getPayAmountRefactor = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+    if (isDead) return 1500;
 
+    if (isSeparated) return 2500;
 
-
+    return isRetired ? 3000 : 4000;
+  };
 })();
