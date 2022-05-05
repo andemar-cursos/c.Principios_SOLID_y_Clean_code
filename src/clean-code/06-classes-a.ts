a045583521ccc9b47bd3892ffa9e73e464cf9dc3
Anderson <andemartinezdev@gmail.com>
2022-05-04 21:54:05 -0500

    26 -> Herencia sin responsabilidad unica
    - Se crearon multiples clases aplicando la herencia, para demostrar (Exageradamente) el no uso de responsabilidad unica.

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (() => {
 
+    // No aplicando el principio de responsabilidad unica,
+
     type Gender = 'M'|'F';
 
     class Person {
@@ -10,9 +12,51 @@
             ) {}
 
     }
+    
+    class User extends Person {
+        public lastAccess: Date;
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ){
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+    
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email:string,
+            role:string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
 
 
-    const newPerson = new Person('Andemar', 'M', new Date('1996, 12, 16'));
-    console.log(newPerson);
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'andemar@not.com',
+        'Admin',
+        'Andemar',
+        'M',
+        new Date('1996-12-16')
+    )
+
+    console.log({userSettings});
 
 })();
